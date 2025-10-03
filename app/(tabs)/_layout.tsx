0f77e6897351995e3be14767e02014817a960149
inteltank1 <inteltank@gmail.com>
2025-10-03 22:22:47 +0200

    Added buttons to bottom of page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,9 +2,11 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
+// import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { FontAwesome6 } from "@react-native-vector-icons/fontawesome6"
+import { Octicons } from "@react-native-vector-icons/octicons"
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -15,21 +17,26 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
+        tabBarShowLabel: false,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarLabelStyle: { fontSize: 1 },
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Launches',
+          tabBarIcon: ({ color }) => <Octicons name="rocket" size={28} color={color}/>,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome6 name="map" size={28} color={color} iconStyle="solid" />,
         }}
       />
     </Tabs>
   );
 }
+
+//
