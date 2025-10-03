9eabad29b2812666e340fd3e88a4489f737c44ac
inteltank1 <inteltank@gmail.com>
2025-10-04 01:38:51 +0200

    the code works... except it only works on android+ios, on web, it directly breaks due to weird metro issues. bummer. fortunately, we wanted phones, and we're getting phones, so nice

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,26 +1,2 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import MapView from 'react-native-maps';
-
-export default function map() {
-  return (
-    <View style={styles.container}>
-      {/*<MapView
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />*/}
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+// app/(tabs)/map.tsx
+export { default } from "./map.native"; // or "./map.web" if you want web-first
---
+++ app/(tabs)/map.web.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Map() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Maps are not supported on web.</Text>
+      </View>
+    );
+}
