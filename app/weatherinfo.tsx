5e96f9a5c30f37b50c402725bc0e57dd45901283
inteltank1 <inteltank@gmail.com>
2025-10-05 14:23:15 +0200

    grabs a few values and calculates mean of weather

--- app/weatherinfo.tsx
+++ app/weatherinfo.tsx
@@ -5,7 +5,10 @@ import axios from 'axios';
 
 const WeatherInfo = () => {
   const { latitude, longitude, year, month, day , hour } = useLocalSearchParams();
-  const [ yearlyT2M, setYearlyT2M ] = useState<{id: number, t2mval: number}[]>([]);
+  const [ yearlyT2M, setYearlyT2M ] = useState<{id: number, t2mval: number, precval: number, uvaval: number}[]>([]);
+  const [ probTemp, setProbTemp ] = useState('Loading...')
+  const [ probRain, setProbRain ] = useState('Loading...')
+  const [ probUV, setProbUV] = useState('Loading...')
 
   function parsetime(yeardifference: number){
     let time = ''
@@ -32,13 +35,11 @@ const WeatherInfo = () => {
     let currentdate = new Date()
 
     // bug: there isn't info until 3 days into the past, so if they put today's date, or yesterday's, the first value will implode. should fix it
-    // TODO: get a bunch of those values and put them in an array
-    // TODO: sleep
 
     let diffToBeginSearch = Number(year) - Number(currentdate.getFullYear())
-    const newYearlyData: {id: number, t2mval: number}[] = [];
+    const newYearlyData: {id: number, t2mval: number, precval: number, uvaval: number}[] = [];
 
-    for (let i = 0; i < 3; i++) { // i is the number of calls to the api
+    for (let i = 0; i < 5; i++) { // i is the number of calls to the api
 
       let time = parsetime(diffToBeginSearch + i)
 
@@ -52,7 +53,7 @@ const WeatherInfo = () => {
 
       console.log("Current Time being searched: " + time)
 
-      let link = 'https://power.larc.nasa.gov/api/temporal/hourly/point?start=' + time + '&end=' + time + '&latitude=36.69899362589028&longitude=-4.439089563723052&community=ag&parameters=T2M&format=json&units=metric&user=IntelTank&header=true'
+      let link = 'https://power.larc.nasa.gov/api/temporal/hourly/point?start=' + time + '&end=' + time + '&latitude='+latitude+'&longitude='+longitude+'&community=ag&parameters=T2M%2CPRECTOTCORR%2CALLSKY_SFC_UVA&format=json&units=metric&user=IntelTank&header=true'
 
       // console.log(link)
 
@@ -60,10 +61,17 @@ const WeatherInfo = () => {
         const response = await axios.get(link);
         let parsedResponse = JSON.parse(response.request.response);
         let t2mValues = parsedResponse.properties.parameter.T2M;
+        let precValues = parsedResponse.properties.parameter.PRECTOTCORR;
+        let uvaValues = parsedResponse.properties.parameter.ALLSKY_SFC_UVA;
+
+        let currenttimeandhour = time.toString() + hourstring.toString()
 
         const newEntry = {
           id: Number(year) - diffToBeginSearch - i,
-          t2mval: t2mValues[time.toString() + hourstring.toString()]
+          t2mval: t2mValues[currenttimeandhour],
+          precval: precValues[currenttimeandhour],
+          uvaval: uvaValues[currenttimeandhour]
+
         };
 
         newYearlyData.push(newEntry);
@@ -87,21 +95,37 @@ const WeatherInfo = () => {
       getinfo();
   }, []);
 
-  function updatechart(values: {id: number, t2mval: number}){
+  const mean = (arr, key) => {
+    const valid = arr.map(item => item[key]).filter(v => v !== undefined && v !== null);
+    return valid.reduce((sum, val) => sum + val, 0) / valid.length;
+  }
+
+  function updatechart(values: {id: number, t2mval: number, precval: number, uvaval: number}){
     console.log(values)
+    console.log("value: "+ values[values.length-1].t2mval)
+
+    let meanT2M = mean(values, 't2mval')
+    let meanPrec = mean(values, 'precval')
+    let meanUVA = mean(values, 'uvaval')
+
+    setProbTemp(meanT2M)
+    setProbRain(meanPrec)
+    setProbUV(meanUVA)
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Information</Text>
-      <View>
+      <Text style={styles.text}>Entered Information</Text>
+      <View style={{marginBottom: 50,}}>
         <Text style={styles.text2}>Latitude: {latitude}</Text>
         <Text style={styles.text2}>Longitude: {longitude}</Text>
-        <Text style={styles.text2}>Max. Temperature:</Text>
-        <Text style={styles.text2}>Min. Temperature:</Text>
-        <Text style={styles.text2}>Humidity:</Text>
-        <Text style={styles.text2}>Wind Speed:</Text>
-        <Text style={styles.text2}>Rain Probability:</Text>
+        <Text style={styles.text2}>Time: {year}-{month}-{day}, hour: {hour}</Text>
+      </View>
+      <Text style={styles.text}>Weather information:</Text>
+      <View>
+        <Text style={styles.text2}>Temperature Probability: {probTemp} Cº</Text>
+        <Text style={styles.text2}>Probability of rain: {probRain} mm/hour</Text>
+        <Text style={styles.text2}>Probable UV Rays: {probUV} MJ/hour</Text>
       </View>
     </View>
   );
