eb5129d5cdc56034c06e259a2553f1436a763e8f
inteltank1 <inteltank@gmail.com>
2025-10-05 12:16:55 +0200

    finally got array working n stuff

--- app/weatherinfo.tsx
+++ app/weatherinfo.tsx
@@ -5,11 +5,10 @@ import axios from 'axios';
 
 const WeatherInfo = () => {
   const { latitude, longitude, year, month, day , hour } = useLocalSearchParams();
+  const [ yearlyT2M, setYearlyT2M ] = useState<{id: number, t2mval: number}[]>([]);
 
-  function parsetime(yeardifference: Number){
-
+  function parsetime(yeardifference: number){
     let time = ''
-
     time = (Number(year) - Number(yeardifference)).toString();
 
     if (Number(month) - 10 < 0) {
@@ -32,53 +31,65 @@ const WeatherInfo = () => {
 
     let currentdate = new Date()
 
-    // TODO: make sure that the day NOR the month NOR the year can exceed the currentdate (-3 days)
+    // bug: there isn't info until 3 days into the past, so if they put today's date, or yesterday's, the first value will implode. should fix it
     // TODO: get a bunch of those values and put them in an array
     // TODO: sleep
 
-    console.log(currentdate)
+    let diffToBeginSearch = Number(year) - Number(currentdate.getFullYear())
+    const newYearlyData: {id: number, t2mval: number}[] = [];
 
-    let time = parsetime(0)
-    let hourstring = ''
+    for (let i = 0; i < 3; i++) { // i is the number of calls to the api
 
-    if (Number(hour) - 10 < 0) {
-      hourstring = '0' + hour.toString()
-    } else {
-      hourstring = hour.toString()
+      let time = parsetime(diffToBeginSearch + i)
+
+      let hourstring = ''
+
+      if (Number(hour) - 10 < 0) {
+        hourstring = '0' + hour.toString()
+      } else {
+        hourstring = hour.toString()
+      }
+
+      console.log("Current Time being searched: " + time)
+
+      let link = 'https://power.larc.nasa.gov/api/temporal/hourly/point?start=' + time + '&end=' + time + '&latitude=36.69899362589028&longitude=-4.439089563723052&community=ag&parameters=T2M&format=json&units=metric&user=IntelTank&header=true'
+
+      // console.log(link)
+
+      try {
+        const response = await axios.get(link);
+        let parsedResponse = JSON.parse(response.request.response);
+        let t2mValues = parsedResponse.properties.parameter.T2M;
+
+        const newEntry = {
+          id: Number(year) - diffToBeginSearch - i,
+          t2mval: t2mValues[time.toString() + hourstring.toString()]
+        };
+
+        newYearlyData.push(newEntry);
+
+        updatechart(newYearlyData)
+
+
+      } catch (error) {
+        console.error(error)
+      }
+
     }
 
-    console.log(time)
-
-    let link = 'https://power.larc.nasa.gov/api/temporal/hourly/point?start=' + time + '&end=' + time + '&latitude=36.69899362589028&longitude=-4.439089563723052&community=ag&parameters=T2M&format=json&units=metric&user=IntelTank&header=true'
-
-    console.log(link)
-
-    try {
-      await axios.get(link).then(function (response) {
-        let parsedResponse = JSON.parse(response.request.response)
-        let t2mValues = parsedResponse.properties.parameter.T2M
-
-        console.log(parsedResponse.properties.parameter.T2M[time.toString() + hourstring.toString()])
-        console.log(parsedResponse.properties.parameter.T2M)
-
-        let temps = Object.values(t2mValues);
-
-        let meanT2M = temps.reduce((sum, val) => sum + val, 0) / temps.length;
-
-        console.log(meanT2M)
-
-
-
-      })
-    } catch (error){
-      console.error(error)
-    }
+    // Update state once with all collected data
+    setYearlyT2M(newYearlyData);
+    console.log("Final yearly data:", newYearlyData);
 
   }
 
   React.useEffect(() => {
       getinfo();
   }, []);
+
+  function updatechart(values: {id: number, t2mval: number}){
+    console.log(values)
+  }
 
   return (
     <View style={styles.container}>
